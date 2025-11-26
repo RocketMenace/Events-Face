258bc0e45096865207db85adbd50dad44c88b9f8
RocketMenace <grizzlygit777@gmail.com>
2025-11-26 05:55:34 +0300

    feat: Implement admin panel functionality

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,1 +1,99 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "is_staff",
+        "is_active",
+        "is_superuser",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("username", "email", "first_name", "last_name")
+    readonly_fields = ("id", "created_at", "updated_at", "date_joined", "last_login")
+    ordering = ("-created_at",)
+    date_hierarchy = "created_at"
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                ),
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (
+            _("Important dates"),
+            {
+                "fields": (
+                    "id",
+                    "created_at",
+                    "updated_at",
+                    "last_login",
+                    "date_joined",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "password1",
+                    "password2",
+                    "email",
+                    "first_name",
+                    "last_name",
+                ),
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+    filter_horizontal = ("groups", "user_permissions")
--- src/events/admin.py
+++ src/events/admin.py
@@ -1,1 +1,86 @@
-# Register your models here.
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from .models import EventAreaModel, EventModel
+
+
+@admin.register(EventAreaModel)
+class EventAreaAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at", "event_count")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("name",)
+    readonly_fields = ("id", "created_at", "updated_at")
+    ordering = ("name",)
+    date_hierarchy = "created_at"
+
+    fieldsets = (
+        (
+            _("Basic Information"),
+            {
+                "fields": ("id", "name"),
+            },
+        ),
+        (
+            _("Timestamps"),
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    def event_count(self, obj):
+        return obj.events.count()
+
+    event_count.short_description = _("Number of Events")
+
+
+class EventAreaInline(admin.TabularInline):
+    model = EventModel
+    extra = 0
+    fields = ("name", "status", "event_datetime")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(EventModel)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "area",
+        "status",
+        "event_datetime",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("status", "area", "event_datetime", "created_at")
+    search_fields = ("name", "area__name")
+    readonly_fields = ("id", "created_at", "updated_at")
+    ordering = ("-event_datetime", "name")
+    date_hierarchy = "event_datetime"
+    autocomplete_fields = ("area",)
+
+    fieldsets = (
+        (
+            _("Basic Information"),
+            {
+                "fields": ("id", "name", "area"),
+            },
+        ),
+        (
+            _("Event Details"),
+            {
+                "fields": ("status", "event_datetime"),
+            },
+        ),
+        (
+            _("Timestamps"),
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related("area")
